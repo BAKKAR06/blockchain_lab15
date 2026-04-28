ea0806c8a85c5952b75510f046c5a760cabfc52d
Bakkar <karabaevbakdaulet16l@gmail.com>
2026-04-28 20:42:19 +0500

    github pages setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/blockchain_lab15",
 };
 
 export default nextConfig;
